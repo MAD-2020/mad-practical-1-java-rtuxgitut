ed91fb097cceb86c880d1e219a73124c4f0407ae
rtuxgitut <64120725+rtuxgitut@users.noreply.github.com>
2020-04-23 23:19:26 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -12,5 +12,10 @@ public class Question3
      
     Scanner in = new Scanner(System.in);
     
+    System.out.print("Enter an integer: ");
+    int num = in.nextInt();
+    
+    System.out.println("Result: " + (num * num));
+    
   }
 }
