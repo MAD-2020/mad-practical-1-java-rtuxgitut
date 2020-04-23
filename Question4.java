55c01c58bf8b5008400f5647ec7e4deba327a11c
rtuxgitut <64120725+rtuxgitut@users.noreply.github.com>
2020-04-23 23:57:29 +0800

    Finished Question 4

--- Question4.java
+++ Question4.java
@@ -18,6 +18,20 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
+
+    //Get Input
+    System.out.print("Enter the integer base of triangle: ");
+    int number = in.nextInt();
+
+    for (int row = 0; row < number; row++)  //Print number of lines based on integer received
+    {
+      for (int ast = 0; ast < (number - row) ; ast++) //Print astericks, -1 going to next row.
+      {
+        System.out.print("*");
+      }
+      System.out.println(); //Go to next line
+    }
     
+    in.close();
   }
 }
