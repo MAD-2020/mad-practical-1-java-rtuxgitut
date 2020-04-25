ac8dd53db3b6a53856d52caec955bfcd0a11fb5a
rtuxgitut <64120725+rtuxgitut@users.noreply.github.com>
2020-04-25 15:36:09 +0800

    Finished Question 5 of Exercise 1

--- Question5.java
+++ Question5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Question5
@@ -27,6 +28,38 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number of integers to input: "); //Ask user for a list of numbers
+    int num = in.nextInt();
+
+    ArrayList<Integer> numList = new ArrayList<Integer>(); //Flexible list to store elements inside
+
+    for(int i = 0; i < num; i++) //System loops x amount of times based on user input
+    {
+      System.out.print("Enter an integer: "); //Enter a number
+      int value = in.nextInt();
+      numList.add(value);
+    }
+    in.close(); //Prevent resource leak
+
+    int mode = 0; //Used to find the mode in the array list
+    int total = 0; //Count number of occurances in the array list
+
+    for(int j = 0; j < num; j++)
+    {
+      int counter = 0; //To compare elements with the same number with one of the numbers in the list (Default value after completing the loop is 1)
+      for(int k = 0; k < num; k++)
+      {
+        if(numList.get(k) == numList.get(j))
+        {
+          counter += 1;
+        }
+      }
+      if(counter > total) //Default value of total is 1
+      {
+        total = counter;
+        mode = numList.get(j); //The iteration with the highest mode will be recorded here and the end of the outer loop the program will print out the mode in the list
+      }
+    }
+    System.out.println(mode);
   }
 }
